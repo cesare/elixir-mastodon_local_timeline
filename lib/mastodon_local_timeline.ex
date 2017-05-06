107a8fd0bcf1540c411c507d463e6897080b736a
SAWADA Tadashi <cesare@mayverse.jp>
2017-05-06 15:08:57 +0900

    Enable to dump local stream in raw JSON

--- lib/mastodon_local_timeline.ex
+++ lib/mastodon_local_timeline.ex
@@ -1,18 +1,46 @@
 defmodule MastodonLocalTimeline do
-  @moduledoc """
-  Documentation for MastodonLocalTimeline.
-  """
+  def start do
+    host = hostname()
+    access_token = access_token()
+    start(host, access_token)
+  end
 
-  @doc """
-  Hello world.
+  def start(host, access_token) do
+    path = request_path(access_token)
+    Socket.Web.connect!(host, path: path, secure: true) |> recv
+  end
 
-  ## Examples
+  defp env(key) do
+    Application.get_env(:mastodon_local_timeline, key)
+  end
 
-      iex> MastodonLocalTimeline.hello
-      :world
+  defp hostname do
+    env(:host)
+  end
 
-  """
-  def hello do
-    :world
+  defp access_token do
+    env(:access_token)
+  end
+
+  defp request_path(access_token) do
+    path = "/api/v1/streaming/"
+    query = request_query(access_token) |> URI.encode_query
+    [path, query] |> Enum.join("?")
+  end
+
+  defp request_query(access_token) do
+    %{
+      access_token: access_token,
+      stream: "public:local"
+    }
+  end
+
+  defp recv(socket) do
+    case socket |> Socket.Web.recv! do
+      {:text, json} -> json |> IO.puts
+      {:ping, message} -> socket |> Socket.Web.send!({:pong, message})
+    end
+
+    recv(socket)
   end
 end
